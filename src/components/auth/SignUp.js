4f343df3a3bf0917ded30e61d4e26fd32d77fc80
Xuthus2022 <readjethro@gmail.com>
2021-11-01 11:49:48 +1300

    Sign Up page

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function SignUp() {
+	const [state, setState] = useState({
+			email: '',
+			password: '',
+			firstName: '',
+			lastNAme: '',
+			phoneNumber: '',
+		}
+	);
+	const handleChange = (e) => {
+		setState({
+			...state,
+			[e.target.id]: e.target.value
+		});
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(state);
+	};
+
+	return (
+		<div className="container">
+			<form onSubmit={handleSubmit} className="white">
+				<h5>Sign Up</h5>
+				<div className="input-field">
+					<label htmlFor="firstName">First Name</label>
+					<input type="text" id="firstName" onChange={handleChange} required/>
+				</div>
+				<div className="input-field">
+					<label htmlFor="lastName">Last Name</label>
+					<input type="text" id="lastName" onChange={handleChange} required/>
+				</div>
+				<div className="input-field"> 
+					<label htmlFor="phoneNumber">Phone Number</label>
+					<input type="tel" id="phoneNumber" pattern="[0-9]{11,14}" placeholder="02245678901" onChange={handleChange} required/>
+				</div>
+				<div className="input-field">
+					<label htmlFor="email">Email</label>
+					<input type="email" id="email" onChange={handleChange} required/>
+				</div>
+				<div className="input-field">
+					<label htmlFor="password">Password</label>
+					<input type="password" id="password" onChange={handleChange} required/>
+				</div>
+				<div className="input-field">
+					<button className="btn pink lighten-1 z-depth-0">Sign up</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
